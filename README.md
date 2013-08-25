449ed0acef194b73cbc2e7865002f0be1b866fd6
root <root@ricardo.com>
2013-08-24 23:37:15 -0300

    added scaffolding for the app

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ tnw
 
 TNW Hack Battle Project
 Mauricio Giordano, Bruno Lemos and Ricardo Parro
+
+To run the project :
+
+1 - Install node
+2 - install volo - npm install -g volo
+3 - on the app directory run:
+a)volo build
+b)volo server
+4 - install the simulator addon on firefox and test it
+
+
---
+++ www/js/app.js
@@ -0,0 +1,83 @@
+
+// This uses require.js to structure javascript:
+// http://requirejs.org/docs/api.html#define
+
+define(function(require) {
+    // Receipt verification (https://github.com/mozilla/receiptverifier)
+    require('receiptverifier');
+
+    // Installation button
+    require('./install-button');
+
+    // Install the layouts
+    require('layouts/layouts');
+
+    // Write your app here.
+
+
+    function formatDate(d) {
+        return (d.getMonth()+1) + '/' +
+            d.getDate() + '/' +
+            d.getFullYear();
+    }
+
+    // List view
+
+    var list = $('.list').get(0);
+    list.add({ title: 'Record a video',
+               desc: 'Here goes the video'});
+    list.add({ title: 'View Map',
+               desc: 'Here goes the map'});
+
+    list.add({ title: 'Timeline',
+                   desc: 'Here goes the Timeline'});
+
+    // Detail view
+
+    var detail = $('.detail').get(0);
+    detail.render = function(item) {
+        $('.title', this).html(item.get('title'));
+        $('.desc', this).html(item.get('desc'));
+        $('.date', this).text(formatDate(item.get('date')));
+    };
+
+    // Edit view
+
+  /*  var edit = $('.edit').get(0);
+    edit.render = function(item) {
+        item = item || { id: '', get: function() { return ''; } };
+
+        $('input[name=id]', this).val(item.id);
+        $('input[name=title]', this).val(item.get('title'));
+        $('input[name=desc]', this).val(item.get('desc'));
+    };
+*/
+    /*edit.getTitle = function() {
+        var model = this.view.model;
+
+        if(model) {
+            return model.get('title');
+        }
+        else {
+            return 'New';
+        }
+    };*/
+
+ /*   $('button.add', edit).click(function() {
+        var el = $(edit);
+        var title = el.find('input[name=title]');
+        var desc = el.find('input[name=desc]');
+        var model = edit.model;
+
+        if(model) {
+            model.set({ title: title.val(), desc: desc.val() });
+        }
+        else {
+            list.add({ title: title.val(),
+                       desc: desc.val(),
+                       date: new Date() });
+        }
+
+        edit.close();
+    });*/
+});
